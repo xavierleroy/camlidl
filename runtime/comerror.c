ccf8a7df855d15125ed6b94448207016de9a8d1b
Xavier Leroy <xavierleroy@users.noreply.github.com>
1999-02-16 18:36:08 +0000

    Ajout

---
+++ runtime/comerror.c
@@ -0,0 +1,149 @@
+/* Error handling */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/callback.h>
+#include "camlidlruntime.h"
+#include "comstuff.h"
+
+static void camlidl_raise_error(HRESULT errcode, value vmsg)
+{
+  static value * com_error_exn = NULL;
+  value bucket;
+
+  if (com_error_exn == NULL) {
+    com_error_exn = caml_named_value("Com.Error");
+    if (com_error_exn == NULL)
+      invalid_argument("Exception Com.Error not initialized");
+  }
+  Begin_root(vmsg)
+    bucket = alloc_small(3, 0);
+    Field(bucket, 0) = *com_error_exn;
+    Field(bucket, 1) = Val_long(errcode);
+    Field(bucket, 2) = vmsg;
+  End_roots();
+  mlraise(bucket);
+}
+
+void camlidl_error(HRESULT errcode, char * msg)
+{
+  camlidl_raise_error(errcode, copy_string(msg));
+}
+
+static void camlidl_hresult_error(HRESULT errcode)
+{
+  /* Build text representation of errcode */
+#ifdef _WIN32
+  LPTSTR msg;
+  value vmsg;
+  interface IErrorInfo * errinfo = NULL;
+  BSTR source, descr;
+  char errmsg[1024];
+
+  if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                      FORMAT_MESSAGE_FROM_SYSTEM,
+                    NULL,       /* message source */
+                    errcode,
+                    0,          /* language */
+                    (LPTSTR) &msg, /* message buffer */
+                    0,          /* max size */
+                    NULL)       /* inserts */
+      != 0) {
+    vmsg = copy_string(msg);
+    LocalFree(msg);
+    camlidl_raise_error(errcode, vmsg);
+  }
+  /* Try to use GetErrorInfo */
+  GetErrorInfo(0L, &errinfo);
+  if (errinfo != NULL) {
+    errinfo->lpVtbl->GetSource(errinfo, &source);
+    errinfo->lpVtbl->GetDescription(errinfo, &descr);
+    _snprintf(errmsg, sizeof(errmsg) - 1, "%ls - %ls", source, descr);
+    errmsg[sizeof(errmsg) - 1] = 0;
+    SysFreeString(source);
+    SysFreeString(descr);
+    errinfo->lpVtbl->Release(errinfo);
+    camlidl_error(errcode, errmsg);
+  } else {
+    camlidl_error(errcode, "Unknown error");
+  }
+#else
+  camlidl_error(errcode, "");
+#endif
+}
+
+void camlidl_check_hresult(HRESULT hr)
+{
+  if (FAILED(hr)) camlidl_hresult_error(hr);
+}
+
+value camlidl_c2ml_hresult_bool(HRESULT hr)
+{
+  if (FAILED(hr)) camlidl_hresult_error(hr);
+  return Val_bool(hr == S_OK);
+}
+
+void camlidl_ml2c_hresult_bool(value v, HRESULT * hr)
+{
+  *hr = Bool_val(v) ? S_OK : S_FALSE;
+}
+
+value camlidl_c2ml_hresult_int(HRESULT hr)
+{
+  if (FAILED(hr)) camlidl_hresult_error(hr);
+  return Val_int(HRESULT_CODE(hr));
+}
+
+void camlidl_ml2c_hresult_int(value v, HRESULT * hr)
+{
+  *hr = MAKE_HRESULT(SEVERITY_SUCCESS, FACILITY_NULL, Int_val(v) & 0xFFFF);
+}
+
+HRESULT camlidl_result_exception(char * methname, value exn_bucket)
+{
+  /* TODO: clever mapping of exception to HRESULTS ? */
+#ifdef _WIN32
+  interface ICreateErrorInfo * createrr;
+  interface IErrorInfo * errinfo;
+  if (SUCCEEDED(CreateErrorInfo(&createrr))) {
+    wstrlen = strlen(methname);
+    wstr = (wchar_t *) malloc((wstrlen + 1) * sizeof(wchar_t));
+    if (wstr != NULL) {
+      mbstowcs(wstr, methname, wstrlen);
+      createrr->lpVtbl->SetSource(createrr, wstr);
+      free(wstr);
+    }
+    exndesc = format_caml_exception(exn_bucket);
+    if (exndesc != NULL) {
+      wstrlen = strlen(exndesc);
+      wstr = (wchar_t *) malloc((wstrlen + 1) * sizeof(wchar_t));
+      if (wstr != NULL) {
+        mbstowcs(wstr, methname, wstrlen);
+        createrr->lpVtbl->SetDescription(createrr, wstr);
+        free(wstr);
+      }
+      free(exndesc);
+    }
+    if (SUCCEEDED(createrr->lpVtbl->QueryInterface(createrr,
+                                                   &IID_IErrorInfo,
+                                                   (void **) &errinfo))) {
+      SeetErrorInfo(0L, errinfo);
+      errinfo->lpVtbl->Release(errinfo);
+    }
+    createrr->lpVtbl->Release(createrr);
+  }
+#endif
+  return MAKE_HRESULT(SEVERITY_FAILURE, FACILITY_ITF, 0x200);
+}
+
+void camlidl_uncaught_exception(char * methname, value exn_bucket)
+{
+  char * msg = format_caml_exception(exn_bucket);
+  fprintf(stderr, "Uncaught exception in COM method %s: %s\n",
+          methname, msg);
+  free(msg);
+  exit(2);
+}
