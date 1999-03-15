3d779e7101ba41ae6a3936bb4aa9d74b0eb8ec37
xleroy <xleroy@a682aa8c-9285-43f4-a502-166e325d51a4>
1999-03-15 16:01:33 +0000

    Debug des dispinterface sous Visual Basic

--- trunk/tests/dispatch/camlcomp.ml
+++ trunk/tests/dispatch/camlcomp.ml
@@ -3,18 +3,18 @@
 class mycomponent =
   object
     method fx =
-      print_string "Camlcomp: fx"; print_newline()
+      (*print_string "Camlcomp: fx"; print_newline(); *) ()
     method fxStringIn str =
       print_string "Camlcomp: fxStringIn "; print_string str; print_newline()
     method fxStringOut =
-      print_string "Camlcomp: fxStringOut "; print_newline();
+      (*print_string "Camlcomp: fxStringOut "; print_newline();*)
       "This string comes from Caml"
     method fxFakeError =
       (failwith "FxFakeError" : unit)
   end
 
 let factory () =
-  print_string "Camlcomp: factory is called"; print_newline();
+  (*print_string "Camlcomp: factory is called"; print_newline();*)
   Component.make_iX(new mycomponent)
 
 let _ =
